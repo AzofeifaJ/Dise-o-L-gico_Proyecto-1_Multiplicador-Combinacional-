4c4225f3cf29ab155096e2a916d4fdfa76ba186f
AndresVA99 <126135460+AndresVA99@users.noreply.github.com>
2023-03-21 23:36:03 -0600

    Update multiplicacionBinaria.py

--- multiplicacionBinaria.py
+++ multiplicacionBinaria.py
@@ -2,8 +2,7 @@ def multiplicacionBinaria(num1,num2):
   #Se obtienen los factores
   PDF=[]
   PDF.append(num1)
-  PDF.append("X  "+num2) 
-  PDF.append("--------")
+  PDF.append(num2) 
   #Se obtiene los productos parciales
   newNum2=num2[::-1]
   for i in range(len(num2)):
@@ -11,18 +10,7 @@ def multiplicacionBinaria(num1,num2):
     for j in range(len(num1)):
       multiplicacion=int(num1[j])*int(newNum2[i])      
       productoParcial=productoParcial+str(multiplicacion)
-    if i==len(num2)-1:
-      k=0
-      while k!=i:
-        productoParcial="+  "+productoParcial+"0"
-        k+=1
-    else:
-      k=0
-      while k!=i:
-        productoParcial=productoParcial+"0"
-        k+=1
-    PDF.append(productoParcial)
-  PDF.append("--------") 
+    PDF.append(productoParcial) 
   #Se obtiene el producto final 
   multiplicar=int(num1,2)*int(num2,2);
   multiBinario=bin(multiplicar)
