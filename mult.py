b5273989d456b576ae0ebbd3cccf3f64227c77ee
AndresVA99 <126135460+AndresVA99@users.noreply.github.com>
2023-03-21 23:37:08 -0600

    Update mult.py

--- mult.py
+++ mult.py
@@ -1,21 +1,22 @@
 from T_Binario import T_Binario
 from multiplicacionBinaria import multiplicacionBinaria
-####from LaTex import Latex
+from generadorPDF import generadorPDF
 def mult(a,b):
-  a=T_Binario(a);
-  b=T_Binario(b);
+  aB=T_Binario(a);
+  bB=T_Binario(b);
   #Si los numeros a multiplicar cumplen con la 
   #cantidad de bits, se realiza la multiplicacion binaria
-  if verificar_Bits(a,b)==True:
-    return multiplicacionBinaria(a,b)
+  if verificar_Bits(aB,bB)==True:
+    pasos=multiplicacionBinaria(aB,bB)
+    return generadorPDF(a,b,pasos)
   else:
-    return verificar_Bits(a,b)  
+    return verificar_Bits(aB,bB)  
 #Verifica que los numeros a multiplicar sean igual 
 #o menos a 8 bits
-def verificar_Bits(a,b):
-  if len(a)>8:
+def verificar_Bits(aB,bB):
+  if len(aB)>8:
     return "El valor <a> excede los 8 bits"
-  elif len(b)>8:
+  elif len(bB)>8:
     return "El valor <b> excede los 8 bits"
   else:
     return True
