562fce1eae9499b163c38668c5ac92cebc401e9c
AndresVA99 <126135460+AndresVA99@users.noreply.github.com>
2023-03-22 16:39:21 -0600

    Update mult.py

--- mult.py
+++ mult.py
@@ -1,6 +1,8 @@
 from T_Binario import T_Binario
 from multiplicacionBinaria import multiplicacionBinaria
 from generadorPDF import generadorPDF
+import sys
+
 def mult(a,b):
   aB=T_Binario(a);
   bB=T_Binario(b);
@@ -20,3 +22,25 @@ def verificar_Bits(aB,bB):
     return "El valor <b> excede los 8 bits"
   else:
     return True
+#Se lee entrada al sistema
+args = sys.argv[1:]
+if args[0] == "--bits":
+    mult(args[3],args[5])
+elif args[0] == "-f":
+    file = open(args[1])
+    linea = list(file.read())
+    #Separamos el texto que entra en una lista para extraer los números a multiplicar
+    listaAux = []
+    stringAux = ""
+    for i in linea:
+      if i == " ":
+        listaAux.append(stringAux)
+        stringAux= ""
+      else:
+        stringAux = stringAux + i
+    listaAux.append(stringAux)
+    mult(listaAux[3],listaAux[5])
+    file.close()
+
+else:
+  print("Comando no correcto")
